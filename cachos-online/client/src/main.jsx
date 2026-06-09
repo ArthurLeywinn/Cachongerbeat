2ac5bee91b7ee45d8b32acc363f9c6b7b0263dcf
jdlynchv <jdlynchv@gmail.com>
2026-06-09 14:40:14 -0400

    Add files via upload

--- cachos-online/client/src/main.jsx
+++ cachos-online/client/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GameProvider } from './context/GameContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { ProfileProvider } from './context/ProfileContext.jsx';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <GameProvider>
-        <App />
-      </GameProvider>
-    </AuthProvider>
+    <ProfileProvider>
+      <AuthProvider>
+        <GameProvider>
+          <App />
+        </GameProvider>
+      </AuthProvider>
+    </ProfileProvider>
   </React.StrictMode>
 );
